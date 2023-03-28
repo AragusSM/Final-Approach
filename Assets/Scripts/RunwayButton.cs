1cbc0f34e2d0ca5be5cb4e4039f390939988e966
Michael <ningzy358@gmail.com>
2023-03-28 16:44:53 -0500

    change the interior

--- Assets/Scripts/RunwayButton.cs
+++ Assets/Scripts/RunwayButton.cs
@@ -7,14 +7,18 @@ public class RunwayButton : MonoBehaviour
     public Runway runWay;
     public bool active;
     
+    public AudioSource planeSound;
+    public AudioSource planeSound2;
 
     public void runwayButtonPress() {
         if (active) {
             runWay.plane.finalStatus();
             if(runWay.plane.departure){
                 runWay.plane.GetComponent<Animator>().Play("TakeOff");
+                planeSound.Play();
             }else{
                 runWay.plane.GetComponent<Animator>().Play("Returning");
+                planeSound2.Play();
             }
             
         }
@@ -22,5 +26,16 @@ public class RunwayButton : MonoBehaviour
 
     void Update() {
         active = runWay.plane && runWay.plane.status == PlaneStatus.Runway;
+        if(runWay.plane && runWay.plane.status == PlaneStatus.Runway){
+            if(runWay.plane.departure){
+                this.transform.GetChild(0).gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = "Clear For TakeOff";
+            }else{
+                this.transform.GetChild(0).gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = "Clear to Terminal";
+
+            }
+        }else{
+            this.transform.GetChild(0).gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = "";
+
+        }
     }
 }
