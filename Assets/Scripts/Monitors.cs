5d28c5a63b599c9950e2b2b5b43eefb1f147fad7
Michael <ningzy358@gmail.com>
2023-04-11 16:01:54 -0500

    Add quit option

--- Assets/Scripts/Monitors.cs
+++ Assets/Scripts/Monitors.cs
@@ -6,15 +6,22 @@ using UnityEngine.SceneManagement;
 public class Monitors : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public bool istimer;
     void Start()
     {
+        if(!istimer){
+            return;
+        }
         this.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = "10:00";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!istimer){
+            return;
+        }
         if(GameManager.state == GameState.AirTrafficControl && GameManager.time_left <= 0)
         {
             InfoHolder.Points = GameManager.score;
@@ -27,5 +34,11 @@ public class Monitors : MonoBehaviour
         string minutes = (((int) GameManager.time_left) / 60).ToString();
         string seconds = (((int) GameManager.time_left) % 60).ToString();
         this.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = minutes + ":" + seconds;
+    }
+
+    public void quitGame(){
+        InfoHolder.Points = GameManager.score;
+        InfoHolder.FailureReason = "You Quit your shift >:(";
+        SceneManager.LoadScene(2);
     }
 }
